600c7eb56e56f177cf8fc16c568f0f750cbbc251
zhangyouyin <zfudijob@163.com>
2019-06-25 15:01:17 +0800

    天剑

---
+++ src/main/java/com/atguigu/jianfa0105/HuaShanJianfa.java
@@ -0,0 +1,10 @@
+package com.atguigu.jianfa0105;
+
+public class HuaShanJianfa {
+    public static void main(String[] args) {
+        System.out.println("huash11an");
+    }
+    public static void math(){
+        System.out.println("huash11an");
+    }
+}
